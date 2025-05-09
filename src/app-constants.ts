1963e70971c7a5b661409c061d3d8e35945f4163
chandrakatore2004 <chandrakatore@gmail.com>
2025-05-09 15:44:06 +0530

    Update app-constants.ts

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants{
 
-    static BACKEND_API_URL= 'http://a5038ef10a41f47f4b90944fb178242e-1093245609.ap-south-1.elb.amazonaws.com/products';
+    static BACKEND_API_URL= 'http://aeaaf39f3be3d4e1994014da3d5d9878-821318660.ap-south-1.elb.amazonaws.com/products';
 }
