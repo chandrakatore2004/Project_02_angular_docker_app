dd62f840a878f8551e4cc09e4a6e8365637db134
chandrakatore2004 <chandrakatore@gmail.com>
2025-05-09 15:30:03 +0530

    Update app-constants.ts

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,3 +1,4 @@
-export class AppConstants {
-    static BACKEND_API_URL = 'http://productssvc.default.svc.cluster.local';
+export class AppConstants{
+
+    static BACKEND_API_URL= 'http://a5038ef10a41f47f4b90944fb178242e-1093245609.ap-south-1.elb.amazonaws.com/products';
 }
